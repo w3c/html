1661fbf04af7240261774394678e9e61675ab5aa
Ade Bateman <bateman@acm.org>
2016-02-10 09:41:56 -0800

    Switch to build from master into gh-pages

--- .deploy-output.sh
+++ .deploy-output.sh
@@ -19,6 +19,6 @@ git add .
 git commit -m "Built by Travis-CI: $STATUS"
 git status
 
-GH_REPO="@github.com/w3c/html-editors-draft.git"
+GH_REPO="@github.com/w3c/html.git"
 FULL_REPO="https://$GH_TOKEN$GH_REPO"
 git push --force --quiet $FULL_REPO master:gh-pages > /dev/null 2>&1
