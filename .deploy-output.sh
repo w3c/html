3f76217d60b669c0b9a1dc03b5cfaf85b38ba841
plehegar <plh@w3.org>
2017-06-08 16:21:58 -0400

    Improved deployement

--- .deploy-output.sh
+++ .deploy-output.sh
@@ -13,6 +13,23 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]
     exit 0
 fi
 
+# Save some useful information
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SHA=`git rev-parse --verify HEAD`
+
+echo $SSH_REPO
+
+# Clone the existing gh-pages for this repo into out/
+# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deploy)
+git clone $REPO out
+cd out
+git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+cd ..
+
+# Clean out existing contents
+rm -rf out/**/* || exit 0
+
 cp single-page.html ./out/
 mkdir ./out/fonts
 mkdir ./out/images
@@ -23,19 +40,24 @@ cp styles/* ./out/styles
 cp entities.dtd ./out/
 cp entities.json ./out/
 
+# Now let's go have some fun with the cloned repo
 cd out
 git init
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
+# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
+if [ -z `git diff --quiet` ]; then
+    echo "No changes to the output on this push; exiting."
+    exit 0
+fi
+
+# Commit the "changes", i.e. the new version.
+# The delta will show diffs between new and old versions.
 git add -A .
 git commit -m "Built by Travis-CI: $STATUS"
 git status
 
-# Save some useful information
-REPO=`git config remote.origin.url`
-SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
-SHA=`git rev-parse --verify HEAD`
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
@@ -48,4 +70,4 @@ eval `ssh-agent -s`
 ssh-add ../deploy_key
 
 # Now that we're all set up, we can push.
-git push --force --quiet $SSH_REPO $TARGET_BRANCH
+git push $SSH_REPO $TARGET_BRANCH
