c1ea696d7538ae7c3c0457b283ec563f0ce4e784
plehegar <plh@w3.org>
2017-06-08 15:52:45 -0400

    Don't use the token anymore

--- .deploy-output.sh
+++ .deploy-output.sh
@@ -2,6 +2,17 @@
 set -ev
 STATUS=`git log -1 --pretty=oneline`
 
+# Partially based on https://gist.github.com/domenic/ec8b0fc8ab45f39403dd
+
+SOURCE_BRANCH="master"
+TARGET_BRANCH="gh-pages"
+
+# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping deploy; just doing a build."
+    exit 0
+fi
+
 cp single-page.html ./out/
 mkdir ./out/fonts
 mkdir ./out/images
@@ -14,13 +25,27 @@ cp entities.json ./out/
 
 cd out
 git init
-git config user.name "Travis-CI"
-git config user.email "travis-ci"
-ls
-git add .
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
+
+git add -A .
 git commit -m "Built by Travis-CI: $STATUS"
 git status
 
-GH_REPO="@github.com/w3c/html.git"
-FULL_REPO="https://$GH_TOKEN$GH_REPO"
-git push --force --quiet $FULL_REPO master:gh-pages
+# Save some useful information
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SHA=`git rev-parse --verify HEAD`
+
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out ../deploy_key -d
+chmod 600 ../deploy_key
+eval `ssh-agent -s`
+ssh-add ../deploy_key
+
+# Now that we're all set up, we can push.
+git push --force --quiet $SSH_REPO $TARGET_BRANCH
